56525f1392f3e6a81034a189059df80e6ad0a65a
Yashwanth M <yashwanth.m@gdn-commerce.com>
2025-06-23 16:35:46 +0530

    changed

--- Basic/main.tf
+++ Basic/main.tf
@@ -2,10 +2,10 @@
 resource local_file sample_res {
 
   filename = var.filename1
-  # content = var.content_value
+   content = var.content_value
   # content = var.boolean_type
   # content = var.list_type[2]
   # content = var.map_type["name"]
-  content = var.tuple_type[0]
+  #content = var.tuple_type[0]
 
 }
