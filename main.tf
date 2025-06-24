94cddda4f8834de0d75efa049f7103000bff896e
Yashwanth M <yashwanth.m@gdn-commerce.com>
2025-06-24 17:02:28 +0530

    Module added

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 
 module "bucket_creation" {
-  source = "./modules/Buckets"
-  project_id = var.project_id
-  zone = var.zone
-  region = var.region
-  credentials = var.credentials
-
+  source               = "./modules/Buckets"
+  project_id           = var.project_id
+  zone                 = var.zone
+  region               = var.region
+  credentials          = var.credentials
   bucket_zone_location = var.bucket_zone_location
-  image_name = var.image_name
-  image_source = var.image_source
+  image_name           = var.image_name
+  image_source         = var.image_source
 }
