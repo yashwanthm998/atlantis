c6cf6805ccb64c46060c65f1f23ceb672dbad02b
Yashwanth M <yashwanth.m@gdn-commerce.com>
2025-06-24 11:01:54 +0530

    path updated

--- GCP_Bucket_Creation/provider.tf
+++ GCP_Bucket_Creation/provider.tf
@@ -11,5 +11,5 @@ provider "google" {
     project = "terraform-gcp-463406"
     zone = "asia-southeast1-a"
     region = "asia-southeast1"
-    credentials = file("home/atlantis/.atlantis/repos/yashwanthm998/atlantis/creds.json")
+    credentials = file("/home/atlantis/.atlantis/repos/yashwanthm998/atlantis/creds.json")
 }
