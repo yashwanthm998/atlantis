cfdeb94f203c4adefd70f8fdeafa83fbb7bac506
Yashwanth M <yashwanth.m@gdn-commerce.com>
2025-06-26 11:15:44 +0530

    Added

--- main.tf
+++ main.tf
@@ -22,37 +22,37 @@ module "bucket_creation" {
   zone                 = var.zone
   region               = var.region
   credentials          = local.credentials
-  bucket = var.module_selector.bucket.enable ? var.module_selector.bucket.instance : []
+  bucket = var.module_selector["bucket"].enable ? var.module_selector["bucket"].instance : []
 }
 
 module "vm_creation" {
-  count        = var.module_selector.vm ? 1 : 0
+  # count        = var.module_selector.vm ? 1 : 0
   source       = "./modules/VM"
   project_id   = local.project_id
   zone         = var.zone
   region       = var.region
   credentials  = local.credentials
-  vm           = var.module_selector.vm.enable ? var.module_selector.vm.instance : []
+  vm           = var.module_selector["vm"].enable ? var.module_selector["vm"].instance : []
 }
 
 module "vpc_creation" {
-  count       = var.module_selector.vpc ? 1 : 0
+  # count       = var.module_selector.vpc ? 1 : 0
   source      = "./modules/VPC"
   project_id  = local.project_id
   zone        = var.zone
   region      = var.region
   credentials = local.credentials
-  vpc =  var.module_selector.vpc.enable ? var.module_selector.vpc.instance : []
+  vpc =  var.module_selector["vpc"].enable ? var.module_selector["vpc"].instance : []
 }
 
 module "service_account_creation" {
-  count       = var.module_selector.sa ? 1 : 0
+  # count       = var.module_selector.sa ? 1 : 0
   source      = "./modules/Service Account"
   project_id  = local.project_id
   zone        = var.zone
   region      = var.region
   credentials = local.credentials
-  sa = var.module_selector.sa.enable ? var.module_selector.sa.instance : []
+  sa = var.module_selector["sa"].enable ? var.module_selector["sa"].instance : []
 }
 
 
