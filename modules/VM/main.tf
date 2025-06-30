a94d0d5251ac93b8d55d1fc651f49d094286efb5
Yashwanth M <yashwanth.m@gdn-commerce.com>
2025-06-30 15:52:18 +0530

    Ansible

--- modules/VM/main.tf
+++ modules/VM/main.tf
@@ -1,25 +1,23 @@
 resource "google_compute_instance" "vm1" {
-  name = var.vm.vm_name
-  zone = var.zone
+  name         = var.vm.vm_name
+  zone         = var.zone
   machine_type = var.vm.machine_type
-  network_interface {
-    network = "default"
-  }
+
   allow_stopping_for_update = true
-  
+
   boot_disk {
     initialize_params {
       image = "rocky-linux-9-v20250611"
-      size = 20
+      size  = 20
     }
   }
-  network_interface {
-  network = "default"
-  access_config {}  
-}
-
-metadata = {
-  ssh-keys = "${file(var.ssh-key)}"
-}
-tags = ["ssh","http-server","https-server"]
+  network_interface{
+    network = "default"
+    access_config {
+    }
+  }
+  metadata = {
+    ssh-keys = "${file(var.ssh-key)}"
+  }
+  tags = ["ssh", "http-server", "https-server"]
 }
