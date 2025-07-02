0b1dab51697c4f051ae89feb356a8f6b81e383bc
Yashwanth M <yashwanth.m@gdn-commerce.com>
2025-07-02 10:39:46 +0530

    Ansible

--- modules/VM/main.tf
+++ modules/VM/main.tf
@@ -22,3 +22,23 @@ metadata = {
 
   tags = ["ssh", "http-server", "https-server"]
 }
+
+
+resource "null_resource" "generate_inventory" {
+  depends_on = [google_compute_instance.vm1]
+
+  provisioner "local-exec" {
+    command = <<EOT
+echo "[gcp]" > ../ansible-using-ssh/hosts
+echo "gcloud-vm-using-atlantis-p1 ansible_host=${google_compute_instance.vm1.network_interface[0].access_config[0].nat_ip} ansible_user=rocky ansible_ssh_private_key_file=/home/atlantis/.atlantis/repos/yashwanthm998/atlantis/ssh_key.pub" >> ../ansible-using-ssh/hosts
+EOT
+  }
+}
+
+resource "null_resource" "run_ansible" {
+  depends_on = [null_resource.generate_inventory]
+
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ../ansible-using-ssh/hosts ../ansible-using-ssh/site.yml"
+  }
+}
