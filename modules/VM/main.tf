5493562a455bec9fa15631b6c5bbab73018955f3
Yashwanth M <yashwanth.m@gdn-commerce.com>
2025-07-02 11:06:05 +0530

    Ansible

--- modules/VM/main.tf
+++ modules/VM/main.tf
@@ -23,9 +23,38 @@ metadata = {
   tags = ["ssh", "http-server", "https-server"]
 }
 
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "allow-ssh"
+  network = "default"
 
-resource "null_resource" "generate_inventory" {
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
+}
+
+
+resource "null_resource" "wait_for_ssh" {
   depends_on = [google_compute_instance.vm1]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "Waiting for SSH to be available..."
+      for i in {1..10}; do
+        nc -zv ${google_compute_instance.vm1.network_interface[0].access_config[0].nat_ip} 22 && break
+        echo "Retrying SSH in 10 seconds..."
+        sleep 10
+      done
+    EOT
+  }
+}
+
+# 📝 Generate Ansible inventory dynamically
+resource "null_resource" "generate_inventory" {
+  depends_on = [null_resource.wait_for_ssh]
 
   provisioner "local-exec" {
     command = <<EOT
@@ -35,10 +64,14 @@ EOT
   }
 }
 
+# ▶️ Run Ansible Playbook
 resource "null_resource" "run_ansible" {
   depends_on = [null_resource.generate_inventory]
 
   provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ../ansible-using-ssh/hosts ../ansible-using-ssh/site.yml"
+    command = <<EOT
+      echo "Running Ansible playbook..."
+      ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ../ansible-using-ssh/hosts ../ansible-using-ssh/site.yml
+    EOT
   }
 }
