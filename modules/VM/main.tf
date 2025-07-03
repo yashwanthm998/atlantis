be32d978e213c3f36fa5e79a5a00498e31a4775a
Yashwanth M <yashwanth.m@gdn-commerce.com>
2025-07-03 15:40:45 +0530

    shell

--- modules/VM/main.tf
+++ modules/VM/main.tf
@@ -23,35 +23,33 @@ metadata = {
 
   tags = ["ssh", "http-server", "https-server"]
 
-# provisioner "remote-exec" {
-#   inline = [ "echo SSH is ready on $(hostname)" ]
 
-#   connection {
-#     type        = "ssh"
-#     user        = each.value.username
-#     private_key = file("/home/atlantis/.atlantis/repos/yashwanthm998/atlantis/ssh")
-#     host        = self.network_interface[0].access_config[0].nat_ip
-#   }
+# provisioner "local-exec" {
+#   command = <<EOT
+#     ip=${self.network_interface[0].access_config[0].nat_ip}
+#     ssh-keygen -R "$ip" || true
+#     echo "${each.value.vm_name} ansible_host=$ip ansible_user=${each.value.username} ansible_ssh_private_key_file=/home/atlantis/.atlantis/repos/yashwanthm998/atlantis/ssh" >> ansible/hosts
+#     echo "Waiting for SSH to be ready on $ip..."
+#     for i in {1..30}; do
+#       ssh -o StrictHostKeyChecking=no -i /home/atlantis/.atlantis/repos/yashwanthm998/atlantis/ssh ${each.value.vm_name}@$ip "echo SSH ready" && break
+#       echo "SSH not ready yet... retrying in 5s"
+#       sleep 5
+#     done
+
+#     ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ansible/hosts ansible/site.yml || true
+#   EOT
 # }
 
-provisioner "local-exec" {
-  command = <<EOT
-    ip=${self.network_interface[0].access_config[0].nat_ip}
-    ssh-keygen -R "$ip" || true
-    echo "${each.value.vm_name} ansible_host=$ip ansible_user=${each.value.username} ansible_ssh_private_key_file=/home/atlantis/.atlantis/repos/yashwanthm998/atlantis/ssh" >> ansible/hosts
-    echo "Waiting for SSH to be ready on $ip..."
-    for i in {1..30}; do
-      ssh -o StrictHostKeyChecking=no -i /home/atlantis/.atlantis/repos/yashwanthm998/atlantis/ssh ${each.value.vm_name}@$ip "echo SSH ready" && break
-      echo "SSH not ready yet... retrying in 5s"
-      sleep 5
-    done
-
-    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ansible/hosts ansible/site.yml || true
-  EOT
 }
 
-
-
+output "vm_info" {
+  value = {
+    for name, vm in google_compute_instance.vm1 :
+    name => {
+      ip       = vm.network_interface[0].access_config[0].nat_ip
+      username = var.vm[0].username
+    }
+  }
 }
 
 
